13f235090b036f92a0399d8a0b0f82c6f0f747e4
henry-ht <henrylapps2@gmail.com>
2021-09-28 21:04:58 -0500

    commit de una

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,6 @@
-import { ErrorInterceptor } from './intercectors/error.interceptor';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatePipe } from './pipes/date.pipe';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RequestInterceptor } from './intercectors/request.interceptor';
-import { ResponseInterceptor } from './intercectors/response.interceptor';
 import { OrderByPipe } from './pipes/order-by.pipe';
 import { SearchPipe } from './pipes/search.pipe';
 
@@ -25,21 +21,7 @@ import { SearchPipe } from './pipes/search.pipe';
     CommonModule
   ],
   providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: RequestInterceptor,
-      multi: true
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ResponseInterceptor,
-      multi: true
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ErrorInterceptor,
-      multi: true
-    },
+
   ]
 })
 export class CoreModule { }
